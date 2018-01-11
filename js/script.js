51ad50275f840abe424f669f465a0a524fc47d70
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2018-01-11 14:04:54 +0100

    Edit script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // scripts.js
 var withButtonClass = document.getElementsByClassName('button');
 var buttonsLength = withButtonClass.length
-for (var i = 0 ; i < withButtonClass.length ; i++) {
+for (var i = 0 ; i < buttonsLength ; i++) {
 	var textContent = withButtonClass[i];
 	alert(textContent.innerHTML);
 }
